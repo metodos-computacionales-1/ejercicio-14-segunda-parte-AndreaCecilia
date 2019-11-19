bb580e23ed25bc5d2a801f221a75a974d5a18070
AndreaCecilia <ac.moreno432@uniandes.edu.co>
2019-11-19 11:53:29 -0500

    Se intento modificar el lambda

--- Euler.cpp
+++ Euler.cpp
@@ -3,26 +3,27 @@
 
 // Defino las constantes del sistema
 const double k=50.0;
-const double lambda = 1;
+const double lambda = 0.5;
 const double m = 2;
-const double gama = 0.3;
+const double gama = 0.9;
 
 double velocidad(double x0, double v0){
   return v0;
 }
-double aceleracion(double x0, double v0, int i=0){
+double aceleracion(double x0, double v0, int i){
   if(i==0){  return -k*pow(x0,lambda)/m;}
-  else{  return -(k*pow(x0,lambda)+gama*v0)/m;}
+  else{return -(k*pow(x0,lambda)+gama*v0)/m;}
 }
 
-void Euler(double & x0, double & v0, double h=0.01, int i=0)
+void Euler(double & x0, double & v0, double h=0.01)
 { x0=x0+h*velocidad(x0, v0); 
 
-  v0=v0+h*aceleracion( x0, v0,i);
+  v0=v0+h*aceleracion( x0, v0,1);
 }
 
-void Runge(double & x0, double & v0, double h=0.01, int i=0)
+void Runge(double & x0, double & v0, double h=0.01)
 {
+  int i=1;
   double k10, k11, k20, k21, k30, k31, k40, k41;
   k10 = h*velocidad(x0, v0);
   k11 = h*aceleracion( x0,  v0, i);
@@ -37,26 +38,31 @@ void Runge(double & x0, double & v0, double h=0.01, int i=0)
   v0 = v0 + (1.0/6.0)*(k11 + 2*k21 + 2*k31 + k41);
 }
 
-int main(int argc, char **argv)
+int main()
+//int argc, char **argv)
 {
   //int N;
   //std::cout<<"Introduce 0 si Euler y 1 si Runge: ";
   //std::cin>>N;
  //Si N es 0 se realiza Euler, si es 1 se da Runge
 
-  int N=atoi(argv[1]); 
-  int ii=atoi(argv[2]);//Define si se usa o no fricción
+  //int N=atoi(argv[1]); 
+  
   double x0=1;
   double v0=0;
   double h=0.01;
   double n=0;
-  for(int i=0; i<1000; i++){
+  for(int i=0; i<100; i++){
 
   std::cout<<n<<" "<<x0<<" "<<v0<<std::endl;
-  if(N==0){Euler(x0, v0, h,ii);}
-  else{  Runge(x0, v0, h,ii);}
-  n=h+n;
-  }
+  //if(N==0){Euler(x0, v0, h);}
+  //else{ 
+  Runge(x0, v0, h);
+  n=h+n;}
+  //
+
   return 0;
 }
+
+
 
