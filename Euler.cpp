dbc4f3f1d521b3f72a274eac8aa8eec321ee329c
AndreaCecilia <ac.moreno432@uniandes.edu.co>
2019-11-19 10:02:26 -0500

    Se crea la funcion que da la segunda derivada

--- Euler.cpp
+++ Euler.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include <cmath>
 
-//const double K=50.0;
-//const double M = 2;
-//const double LAMBDA = 1;
-//const double DeltaT = 0.01;
+// Defino las constantes del sistema
+const double k=50.0;
+const double lambda = 1;
+const double m = 2;
+
+
 double velocidad(double x0, double v0){
   return v0;
 }
+double aceleracion(double x0, double v0){
+  return -k*pow(x0,lambda)/m;
+}
 
+//const double DeltaT = 0.01;
 int main(void)
 {
 
