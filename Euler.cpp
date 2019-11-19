2b911ef86fc52893bab469ac9d048f57014b6497
AndreaCecilia <ac.moreno432@uniandes.edu.co>
2019-11-19 09:59:00 -0500

    Se crea la funcion que da la primera derivada

---
+++ Euler.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+//const double K=50.0;
+//const double M = 2;
+//const double LAMBDA = 1;
+//const double DeltaT = 0.01;
+double velocidad(double x0, double v0){
+  return v0;
+}
+
+int main(void)
+{
+
+  //const double T_init=0;
+  //const double T_final=17.0;
+  //double x=1;
+  //double v=0;
+  
+  return 0;
+}
+
+
